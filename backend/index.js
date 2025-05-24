d72aa064d5f07f14fb366eab78933074ce655e57
Shantanu-121 <143492642+Shantanu-121@users.noreply.github.com>
2025-05-25 02:57:15 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,13 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
+app.use(cors(
+  {
+    origin: ["https://popx-frontend-mu.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true
+  }
+));
 app.use(express.json());
 
 mongoose
